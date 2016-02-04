dfbc6349154339b1977f1142e0240469ff1334b3
sushankkdahiwadkar <sushankdahiwadkar@gmail.com>
2016-02-04 23:51:15 +0530

    added DatabaseHandler.java
    http://www.androidhive.info/2011/11/android-sqlite-database-tutorial/

---
+++ LoginRegisterMaterial/app/src/main/java/com/sushank/loginregistermaterial/adapter/DatabaseHandler.java
@@ -0,0 +1,7 @@
+package com.sushank.loginregistermaterial.adapter;
+
+/**
+ * Created by SushankKDahiwadkar on 2/4/2016.
+ */
+public class DatabaseHandler {
+}
---
+++ LoginRegisterMaterial/app/src/main/java/com/sushank/loginregistermaterial/model/User.java
@@ -0,0 +1,7 @@
+package com.sushank.loginregistermaterial.model;
+
+/**
+ * Created by SushankKDahiwadkar on 2/4/2016.
+ */
+public class User {
+}
