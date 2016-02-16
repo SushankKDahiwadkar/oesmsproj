8ffe1a9d81f31b36850bdd0fe1c59d6f4c45d149
Sushank Dahiwadkar <sushank_dahiwadkar@persistent.com>
2016-02-16 19:07:31 +0530

    minor commit

--- vms/src/main/java/com/psl/vms/controller/VisitorController.java
+++ vms/src/main/java/com/psl/vms/controller/VisitorController.java
@@ -5,17 +5,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
